b51309bd42beef1b0c755977c06b96e6f85b99d0
Ushi Hiraga <enanonegro2015@gmail.com>
2022-04-29 17:02:21 -0500

    Cambios en la estructura de archivos.

--- scripts/main_nhentai.js
+++ scripts/main_nhentai.js
@@ -1,17 +1,5 @@
-import * as proba from "./worker.js"
-const main_image = document.querySelector("img.lazyload");
-function Main() {
-    console.log(main_image);
-    console.log(proba.hola);
+function Main(){
+    console.log("Hola");
 }
-function GetMetadata() {
-    var _a;
-    const scripts_tags = Array.from(document.querySelectorAll("script"));
-    let data_script = (_a = scripts_tags.find((e) => e.innerText.includes("window._gallery"))) === null || _a === void 0 ? void 0 : _a.innerText;
-    data_script = data_script === null || data_script === void 0 ? void 0 : data_script.replace("window._gallery = JSON.parse(\"", "").replace("\");", "");
-    data_script = data_script === null || data_script === void 0 ? void 0 : data_script.replace(/\\u0022/g, "\"").replace(/\n/g, "").replace(/\t/g, "");
-    let data_json = JSON.parse(String(data_script));
-    return data_json;
-}
+
 Main();
-export {};
