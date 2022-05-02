50791ff7f2fd208370b0acfcb563b682f098d653
Ushi Hiraga <enanonegro2015@gmail.com>
2022-05-01 22:36:10 -0500

    Cambio en la forma de acceder a las imagenes.

--- scripts/service_worker.js
+++ scripts/service_worker.js
@@ -1,5 +1,13 @@
 function InstallEventHandler() {
+    const default_configuration = {
+        "generateMethod": "normal",
+        "autoDownload": false,
+        "maxAttemptNumber": 10,
+    };
+
+    chrome.storage.local.set({"config": default_configuration});
     console.log("La extensión ha sido instalada.");
+    return true;
 };
 
 function MessageEventHandler(message, sender, sendResponse) {
@@ -8,7 +16,7 @@ function MessageEventHandler(message, sender, sendResponse) {
         const new_tab_propierties = {
             active: false,
             openerTabId: id_sender,
-            url: "https://i.nhentai.net/descargando-doujin"
+            url: "https://i.nhentai.net/generando-doujin"
         };
 
         await chrome.storage.local.set({ ["request_from_tab_" + id_sender]: message.data })
@@ -17,16 +25,16 @@ function MessageEventHandler(message, sender, sendResponse) {
     };
 
     async function ReturnTabInfo() {
-        const id_opener = sender.tab.openerTabId
-        if(id_opener) sendResponse(id_opener);
-    }
+        const id_parent_tab = sender.tab.openerTabId;
+        if (id_parent_tab) sendResponse(id_parent_tab);
+        else sendResponse(null);
+    };
 
     switch (message.code) {
         case "generate-download-doujin": CreateTabAndSaveData(); break;
         case "who-is-my-parent": ReturnTabInfo(); break;
-    }
-}
-
+    };
+};
 
 chrome.runtime.onInstalled.addListener(InstallEventHandler);
 chrome.runtime.onMessage.addListener(MessageEventHandler);
