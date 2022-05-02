5fed6c4f81023a361bdf7f2acd5e7706eeb064cf
Ushi Hiraga <enanonegro2015@gmail.com>
2022-05-02 01:29:16 -0500

    Ya se añade la imagen del final automaticamente.

--- scripts/main_ihentai.js
+++ scripts/main_ihentai.js
@@ -7,10 +7,8 @@ function MultipleLoadImage(imageUrl) {
     function onSuccessHandler() {
         this.removeEventListener("load", onSuccessHandler);
         this.removeEventListener("error", onErrorHandler);
-        console.log("Image has been loaded.");
-
-        const finishedEvent = new CustomEvent("DoujinImageLoaded", { detail: this })
-        window.dispatchEvent(finishedEvent);
+        window.dispatchEvent(new Event("DoujinImageLoaded"));
+        console.log("Image has loaded.");
         return true;
     };
 
@@ -18,13 +16,12 @@ function MultipleLoadImage(imageUrl) {
         if (this.currentAttempt > this.maxNumberAttempts) {
             this.removeEventListener("load", onSuccessHandler);
             this.removeEventListener("error", onErrorHandler);
-            const finishedEvent = new CustomEvent("DoujinImagesFailed", { detail: this });
-            window.dispatchEvent(finishedEvent);
+            window.dispatchEvent(new Event("DoujinImagesFailed"));
             throw new Error("Maximum number of attempts exceeded.");
         }
 
         console.warn(`Attempt ${this.currentAttempt} has failed. Retrying.`)
-        this.load();
+        load();
         return true;
     }
 
@@ -33,61 +30,54 @@ function MultipleLoadImage(imageUrl) {
     imageElement.maxNumberAttempts = 10;
     imageElement.currentAttempt = 0;
     imageElement.load = load;
-    imageElement.onSuccessHandler = onSuccessHandler;
-    imageElement.onErrorHandler = onErrorHandler;
-
     imageElement.addEventListener("load", onSuccessHandler);
     imageElement.addEventListener("error", onErrorHandler);
 
     return imageElement;
 }
 
-function SaveFailedImages(event) {
-    console.log(event);
-    window["failedImages"].push(event.detail.src);
+function closeDoujinAndPrint() {
+    const last_page_url = `/resources/pages/${chrome.i18n.getMessage("language_code")}/last_page.png`;
+
+    const last_page_element = document.createElement("img");
+    last_page_element.classList.add("doujin_page_image", "vertical");
+    last_page_element.setAttribute("src", chrome.runtime.getURL(last_page_url));
+    document.body.appendChild(last_page_element);
+
+    last_page_element.addEventListener("load", () => window.print());
+    return true;
+
 }
 
+function loadNextImage() {
+    window.last_image_index++;
+    if (window.last_image_index > doujin_data.pages.length) {
+        closeDoujinAndPrint();
+        return true;
+    };
 
+    const base_link = "https://i.nhentai.net";
+    const page_data = doujin_data.pages[window.last_image_index - 1];
+    const url = `${base_link}/galleries/${doujin_data.repo_id}/${window.last_image_index}.${page_data.type}`;
+
+    const image_element = MultipleLoadImage(url);
+    image_element.classList.add("doujin_page_image", page_data.orientation);
+    image_element.load();
+    document.body.appendChild(image_element);
+    return true;
+};
 
 async function Main() {
-    function nextImagedLoad(e) {
-        console.log(e);
-        window.lastImageLoadedIndex++;
-
-        const BASE_LINK = "https://i.nhentai.net/galleries";
-        const actual = window.lastImageLoadedIndex;
-        if (actual > doujin_data.pages.length) return true;
-        console.log(actual);
-        const pageData = doujin_data.pages[window.lastImageLoadedIndex - 1];
-        const orientation = pageData.orientation;
-        const url = `${BASE_LINK}/${doujin_data.repo_id}/${actual}.${pageData.type}`
-        console.log(url);
-        const image_element = MultipleLoadImage(url);
-        image_element.classList.add("doujin_page_image");
-
-        image_element.classList.add((orientation == "horizontal") ? "horizontal" : "vertical")
-
-
-        document.body.appendChild(image_element);
-        image_element.load();
-        return true;
-
-    }
     const parent_tab = await chrome.runtime.sendMessage({ code: "who-is-my-parent" });
     const storage_data = await chrome.storage.local.get(null);
-    const doujin_data = storage_data["request_from_tab_" + parent_tab].doujin_information;
+
+    window["last_image_index"] = 0;
+    window["doujin_data"] = storage_data["request_from_tab_" + parent_tab];
+    window.addEventListener("DoujinImageLoaded", loadNextImage);
 
     document.body.classList.add("body_printable");
     document.title = doujin_data.media_id + "_" + doujin_data.title;
-    window.lastImageLoadedIndex = 0;
-    console.log(doujin_data);
-
-    window.addEventListener("DoujinImagesFailed", SaveFailedImages);
-    window.addEventListener("DoujinImageLoaded", nextImagedLoad)
-    nextImagedLoad();
-
-}
-
-
+    loadNextImage();
+};
 
 Main();
